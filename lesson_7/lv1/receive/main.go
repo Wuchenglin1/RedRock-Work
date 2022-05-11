fdb2a9dfee643cf299ba838414522d6fc0da0419
Wuchenglin1 <1336636739@qq.com>
2022-05-11 09:11:35 +0800

    第七次-lv1-吴承霖-2021214174

---
+++ lesson_7/lv1/receive/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+const account = "amqp://admin:wcl2021214174..@110.42.184.72:5672/"
+
+func main() {
+	receiveMsg()
+}
+
+func receiveMsg() {
+	conn, err := amqp.Dial(account)
+	if err != nil {
+		log.Fatalf("连接rabbitmq失败: %v", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("打开管道失败: %v", err)
+	}
+	defer ch.Close()
+
+	chName := "ch1"
+	q, err := ch.QueueDeclare(chName, false, false, false, false, nil)
+	if err != nil {
+		log.Fatalf("声明队列失败: %v", err)
+	}
+	msgs, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		log.Fatalf("注册失败: %v", err)
+	}
+
+	exitCh := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			fmt.Printf("从 %v 接收到消息 %v \n", chName, string(d.Body))
+		}
+	}()
+	fmt.Printf("正在从 %v 接收消息\n", chName)
+	<-exitCh
+}
---
+++ lesson_7/lv1/send/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+const account = "amqp://admin:wcl2021214174..@110.42.184.72:5672/"
+
+func main() {
+	sendMsg()
+}
+
+func sendMsg() {
+	conn, err := amqp.Dial(account)
+	if err != nil {
+		log.Fatalf("连接rabbitmq失败 : %v", err)
+	}
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("打开管道失败: %v", err)
+	}
+	defer ch.Close()
+
+	chName := "ch1"
+	q, err := ch.QueueDeclare(chName, false, false, false, false, nil)
+	if err != nil {
+		log.Fatalf("声明队列失败: %v", err)
+	}
+
+	var body string
+	fmt.Println("请输入想发送的内容")
+	fmt.Scanln(&body)
+
+	err = ch.Publish("", q.Name, false, false, amqp.Publishing{ContentType: "text/plain", Body: []byte(body)})
+	if err != nil {
+		log.Fatalf("发布消息失败: %v", err)
+	}
+	log.Fatalf("向 %v 发送消息 %v 成功!", chName, body)
+}
